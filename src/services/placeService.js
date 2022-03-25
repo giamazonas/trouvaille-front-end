ba838b88536989cee564b60543e41871f7042893
maxmay94 <maxmay94@gmail.com>
2022-03-25 13:11:18 -0400

    places adjustments, almost done iwth addPlace'

--- src/services/placeService.js
+++ src/services/placeService.js
@@ -1,6 +1,6 @@
 import * as tokenService from '../services/tokenService'
-
-const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/places`
+// const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/places`
+const BASE_URL = "/api/cities";
 
 async function getAllPlaces() {
   const res = await fetch(BASE_URL, {
@@ -9,4 +9,42 @@ async function getAllPlaces() {
   return await res.json()
 }
 
-export { getAllPlaces }
+function create(place) {
+  return fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: place
+  })
+  .then(res => res.json())
+}
+
+function deleteOne(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+  })
+  .then(res => res.json())
+}
+
+function update(place) {
+  return fetch(`${BASE_URL}/${place.get('._id')}`, {
+    method: 'PUT',
+    headers: {
+      
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: place
+  })
+  .then(res => res.json())
+}
+
+export { 
+  getAllPlaces,
+  create,
+  deleteOne,
+  update
+}

0b93df827e143a20f711334df561772d961cf80c
giamazonas <skibinskirenee@gmail.com>
2022-03-26 11:00:28 -0400

    city id link

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -9,7 +9,14 @@ function CityCard({city, handleDeleteCity}) {
     className="card-img-top" 
     />
     <div className="card-body">
-      <h2 className="card-text">{city.city}</h2>
+      <h2 className="card-text">
+      <Link
+
+        className='btn btn-sm btn-warning'
+        to={`/${city._id}`}
+        state={{city}}
+        
+      >{city.city}</Link></h2>
       <p className="card-text">{city.state}</p>
     </div>
     <div className="card-footer">
--- src/pages/CityId/CityId.jsx
+++ src/pages/CityId/CityId.jsx
@@ -1,9 +1,51 @@
 // import { useState, useRef, useEffect } from 'react'
 import AddItinerary from '../../components/AddItinerary/AddItinerary'
+import * as cityService from '../../services/cities'
+import { useState, useEffect } from 'react'
+import { useParams, useLocation, Link } from 'react-router-dom'
 
 const CityId = (props) => {
+  const [cityDetails, setCityDetails] = useState({})
+  let location = useLocation()
+
+  // const {} = useParams()
+
+  // useEffect(() => {
+  //   getCitydetails(location.state.city._id)
+  //   .then(cityData => setCityDetails(cityData))
+  // })
+
+  useEffect(() => {
+    cityService.getAll()
+    .then(allCities => setCityDetails(allCities))
+  }, [])
+
   return (
-    console.log('EACH CITY', CityId)
+    <>
+      <div className='mapbox-container'>
+
+      </div>
+      <div className='places-container'>
+
+      </div>
+      <div className='city-container'>
+        {cityDetails.city._id ?
+        <>
+          <h4 className='city-details'>{cityDetails.city}</h4>
+          {cityDetails.desc}
+          {cityDetails.population}
+          {cityDetails.walkable}
+        </>
+        :
+        <>
+          <h2>Loading City Details...</h2>
+        </>
+        }
+      </div>
+      <div className='itinerary-container'>
+
+      </div>
+    </>
   );
 }
 

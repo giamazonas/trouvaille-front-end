db91ba701a229028129413eaca91c5864a476f89
Garam Jang <garamj88@gmail.com>
2022-03-27 12:03:51 -0400

    add icon to SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import styles from './SearchBar.module.css'
 
 const SearchBar = ({ placeholder, data }) => {
+  
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} />
-        <div className="searchIcon"></div>
+        <input className={styles.inputText} type="text" placeholder={placeholder} />
+        <button className={styles.searchIcon}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
       </div>
-      <div className="searchResult"></div>
+      <div className="searchResult">
+        {/* {data.map((place, k) => {
+          return <div>{place.name}</div>
+        })} */}
+      </div>
     </div>
   );
 }

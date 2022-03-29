60ca6228cb675acaded6932025bb0bdbe4137ad8
Garam Jang <garamj88@gmail.com>
2022-03-29 00:52:43 -0400

    add styling to cityId & card

--- src/pages/CityList/CityList.jsx
+++ src/pages/CityList/CityList.jsx
@@ -4,21 +4,25 @@ import CityCard from '../../components/CityCard/CityCard'
 
 const CityList = props => {
 
-  return (  
-    <>
-      <h1>Cities</h1>
-      <div className={styles.container}>
-        {props.cities.map(city => ( 
-          <CityCard 
-            key={city._id}
-            city={city}
-            // insert photo link
-            handleUpdateCity={props.handleUpdateCity}
-            handleDeleteCity={props.handleDeleteCity}
-            /> 
-        ))}
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Cities</h2>
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {props.cities.map(city => (
+            <div className="group relative">
+              <CityCard
+              key={city._id}
+              city={city}
+              // insert photo link
+              handleUpdateCity={props.handleUpdateCity}
+              handleDeleteCity={props.handleDeleteCity}
+            />
+            </div>
+          ))}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 

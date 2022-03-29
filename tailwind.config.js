e08fedef9f1774fa77230b75fce286db84d641a8
Garam Jang <garamj88@gmail.com>
2022-03-29 11:41:58 -0400

    tailwind quick fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    require(
+      '@tailwindcss/aspect-ratio',
+      '@tailwindcss/forms'),
   ],
 }

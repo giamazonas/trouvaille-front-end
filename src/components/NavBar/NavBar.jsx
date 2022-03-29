64fdfc855b2df3872ee4ef18fa7a13486bfdffa6
Garam Jang <garamj88@gmail.com>
2022-03-29 18:00:02 -0400

    revise navBar & landing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -25,8 +25,8 @@ const NavBar = ({ user, handleLogout }) => {
         <Popover className="relative bg-white">
           <div className="h-full mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-              <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="/"> <span>Trouvaille</span> </a>
+              <div className="text-xl flex justify-start lg:w-0 lg:flex-1">
+                <a href="/">Trouvaille</a>
               </div>
               <div className="-mr-2 -my-2 md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
@@ -250,7 +250,7 @@ const NavBar = ({ user, handleLogout }) => {
             <Popover.Panel focus className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
               <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                 <div className="pt-5 pb-6 px-5">
-                  <div className="text-2xl flex items-center justify-between">
+                  <div className="flex items-center justify-between">
                     <div><span>Trouvaille</span></div>
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
@@ -323,8 +323,8 @@ const NavBar = ({ user, handleLogout }) => {
         <Popover className="relative bg-white">
           <div className="h-full mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-              <div className="text-2xl flex justify-center lg:w-0 lg:flex-1">
-                Trouvaille
+              <div className="text-xl flex justify-center lg:w-0 lg:flex-1">
+                <a href="/">Trouvaille</a>
               </div>
             </div>
           </div>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -14,6 +14,15 @@ const Landing = ({ user }) => {
     <h1>Trouvaille</h1>
     <h1>/tro͞o`vī/</h1>
     <h3>Please sign in to encounter the best day of your life</h3>
+    <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                Sign in
+              </a>
+              <a
+                href="/signup"
+                className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-500 hover:bg-gray-400"
+              >
+                Sign up
+              </a>
     </div>
     }
     </main>

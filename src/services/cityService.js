8eb48e045e02701a9c3c66a9df3273f1bcf6ed82
giamazonas <skibinskirenee@gmail.com>
2022-03-30 17:27:47 -0400

    remove admin

--- src/services/cityService.js
+++ src/services/cityService.js
@@ -2,8 +2,8 @@ import * as tokenService from "../services/tokenService";
 const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/cities`;
 
 function create(city) {
-  console.log(city);
-  return fetch(BASE_URL, {
+  console.log('OOOOOOOO', city);
+  return fetch(`${BASE_URL}`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${tokenService.getToken()}`,

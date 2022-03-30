172a15eb5d80c9c96613a503e223415a993009ac
Garam Jang <garamj88@gmail.com>
2022-03-30 01:24:22 -0400

    update messages on login and signup

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -64,7 +64,7 @@ const LoginForm = props => {
       </div>
 
       <div>
-        <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Log In</button>
+        <button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Sign in</button>
       </div>
 
       <div className="flex items-center justify-between">
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -12,25 +12,8 @@ const Signup = props => {
 
   return (
     <>
-      <div className="hidden sm:block" aria-hidden="true">
-        <div className="py-5">
-        </div>
-      </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:grid md:grid-cols-3 md:gap-6">
-        <div className="md:col-span-1">
-          <div className="px-4 sm:px-0">
-            <h1 className="text-xl font-medium leading-6 text-gray-900">Create an Account</h1>
-            <br></br>
-            <p className="mt-1 text-md text-gray-600">Welcome to Trouvaille!</p>
-            <p className="mt-1 text-md text-gray-600">You're just a step away from encountering your best day of your life!</p>
-            <p className="mt-1 text-sm text-gray-600">{message}</p>
-            <br></br>
-            <Link to="/" className="underline underline-offset-2 hover:font-bold">Go back to main</Link>
-          </div>
-        </div>
-        <SignupForm {...props} updateMessage={updateMessage} />
-      </div>
-
+      <p className="mt-1 text-sm text-gray-600">{message}</p>
+      <SignupForm {...props} updateMessage={updateMessage} />
     </>
   )
 }
